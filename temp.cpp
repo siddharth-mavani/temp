9f87d010ab5879429ba5a4ef50db4bcea1c3a69f
siddharth-mavani <siddharthmavani555@gmail.com>
2023-11-29 23:36:04 +0530

    new file temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define eps 1e-8
+#define PI 3.141592653589793
+#define bsize 512
+
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define pb push_back
+#define bs 1000000007
+
+using namespace std;
+
+int main()
+{
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        
+    cout << "Hello World!";
+
+    return 0;
+}
